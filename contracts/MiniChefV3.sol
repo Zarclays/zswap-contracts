8fb3c629bdc20d3579edef2761765ea18f5b5936
layinka <xcelsis02@gmail.com>
2024-08-15 22:04:30 +0100

    Add MiniChefV3

---
+++ contracts/MiniChefV3.sol
@@ -0,0 +1,353 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+
+
+// solhint-disable avoid-low-level-calls
+// solhint-disable no-inline-assembly
+
+// WARNING!!!
+// Combining BoringBatchable with msg.value can cause double spending issues
+// https://www.paradigm.xyz/2021/08/two-rights-might-make-a-wrong/
+// Original BoringCrypto IERC20
+interface IBoringERC20 {
+    // transfer and tranferFrom have been removed, because they don't work on all tokens (some aren't ERC20 complaint).
+    // By removing them you can't accidentally use them.
+    // name, symbol and decimals have been removed, because they are optional and sometimes wrongly implemented (MKR).
+    // Use BoringERC20 with `using BoringERC20 for IERC20` and call `safeTransfer`, `safeTransferFrom`, etc instead.
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /// @notice EIP 2612
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
+
+contract BaseBoringBatchable {
+    error BatchError(bytes innerError);
+
+    /// @dev Helper function to extract a useful revert message from a failed call.
+    /// If the returned data is malformed or not correctly abi encoded then this call can fail itself.
+    function _getRevertMsg(bytes memory _returnData) internal pure{
+        // If the _res length is less than 68, then
+        // the transaction failed with custom error or silently (without a revert message)
+        if (_returnData.length < 68) revert BatchError(_returnData);
+
+        assembly {
+            // Slice the sighash.
+            _returnData := add(_returnData, 0x04)
+        }
+        revert(abi.decode(_returnData, (string))); // All that remains is the revert string
+    }
+
+    /// @notice Allows batched call to self (this contract).
+    /// @param calls An array of inputs for each call.
+    /// @param revertOnFail If True then reverts after a failed call and stops doing further calls.
+    // F1: External is ok here because this is the batch function, adding it to a batch makes no sense
+    // F2: Calls in the batch may be payable, delegatecall operates in the same context, so each call in the batch has access to msg.value
+    // C3: The length of the loop is fully under user control, so can't be exploited
+    // C7: Delegatecall is only used on the same contract, so it's safe
+    function batch(bytes[] calldata calls, bool revertOnFail) external payable {
+        for (uint256 i = 0; i < calls.length; i++) {
+            (bool success, bytes memory result) = address(this).delegatecall(calls[i]);
+            if (!success && revertOnFail) {
+                _getRevertMsg(result);
+            }
+        }
+    }
+}
+
+contract BoringBatchable is BaseBoringBatchable {
+    /// @notice Call wrapper that performs `ERC20.permit` on `token`.
+    /// Lookup `IERC20.permit`.
+    // F6: Parameters can be used front-run the permit and the user's permit will fail (due to nonce or other revert)
+    //     if part of a batch this could be used to grief once as the second call would not need the permit
+    function permitToken(
+        IBoringERC20 token,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        token.permit(from, to, amount, deadline, v, r, s);
+    }
+}
+
+
+contract BoringOwnableData {
+    address public owner;
+    address public pendingOwner;
+}
+
+contract BoringOwnable is BoringOwnableData {
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /// @notice `owner` defaults to msg.sender on construction.
+    constructor() {
+        owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    /// @notice Transfers ownership to `newOwner`. Either directly or claimable by the new pending owner.
+    /// Can only be invoked by the current `owner`.
+    /// @param newOwner Address of the new owner.
+    /// @param direct True if `newOwner` should be set immediately. False if `newOwner` needs to use `claimOwnership`.
+    /// @param renounce Allows the `newOwner` to be `address(0)` if `direct` and `renounce` is True. Has no effect otherwise.
+    function transferOwnership(
+        address newOwner,
+        bool direct,
+        bool renounce
+    ) public onlyOwner {
+        if (direct) {
+            // Checks
+            require(newOwner != address(0) || renounce, "Ownable: zero address");
+
+            // Effects
+            emit OwnershipTransferred(owner, newOwner);
+            owner = newOwner;
+            pendingOwner = address(0);
+        } else {
+            // Effects
+            pendingOwner = newOwner;
+        }
+    }
+
+    /// @notice Needs to be called by `pendingOwner` to claim ownership.
+    function claimOwnership() public {
+        address _pendingOwner = pendingOwner;
+
+        // Checks
+        require(msg.sender == _pendingOwner, "Ownable: caller != pending owner");
+
+        // Effects
+        emit OwnershipTransferred(owner, _pendingOwner);
+        owner = _pendingOwner;
+        pendingOwner = address(0);
+    }
+
+    /// @notice Only allows the `owner` to execute the function.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not the owner");
+        _;
+    }
+}
+
+contract MiniChef is BoringOwnable, ReentrancyGuard, BoringBatchable {
+    using SafeERC20 for IERC20;
+
+    struct UserInfo {
+        uint256 amount;
+        int256 rewardDebt;
+    }
+
+    struct PoolInfo {
+        uint256 accRewardPerShare;
+        uint256 lastRewardTime;
+        uint256 allocPoint;
+    }
+
+    IERC20 public immutable REWARD;
+
+    PoolInfo[] public poolInfo;
+    IERC20[] public lpToken;
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    uint256 public totalAllocPoint;
+    uint256 public rewardPerSecond;
+    uint256 private constant ACC_REWARD_PRECISION = 1e12;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
+    event Harvest(address indexed user, uint256 indexed pid, uint256 amount);
+    event LogPoolAddition(uint256 indexed pid, uint256 allocPoint, IERC20 indexed lpToken);
+    event LogSetPool(uint256 indexed pid, uint256 allocPoint);
+    event LogUpdatePool(uint256 indexed pid, uint256 lastRewardTime, uint256 lpSupply, uint256 accRewardPerShare);
+    event LogRewardPerSecond(uint256 rewardPerSecond);
+
+    constructor(IERC20 _reward, uint256 _rewardPerSecond, address owner) {
+        REWARD = _reward;
+        rewardPerSecond = _rewardPerSecond;
+        transferOwnership(owner, true,false);
+    }
+
+    function poolLength() public view returns (uint256 pools) {
+        pools = poolInfo.length;
+    }
+
+    function add(uint256 allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardTime = block.timestamp;
+        totalAllocPoint = totalAllocPoint + allocPoint;
+        lpToken.push(_lpToken);
+        poolInfo.push(PoolInfo({
+            allocPoint: allocPoint,
+            lastRewardTime: lastRewardTime,
+            accRewardPerShare: 0
+        }));
+        emit LogPoolAddition(lpToken.length - 1, allocPoint, _lpToken);
+    }
+
+    /// @notice Update the given pool's REWARD allocation point. Can only be called by the owner.
+    /// @param _pid The index of the pool. See `poolInfo`.
+    /// @param _allocPoint New AP of the pool.
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint - poolInfo[_pid].allocPoint + _allocPoint;
+        poolInfo[_pid].allocPoint = _allocPoint;
+        emit LogSetPool(_pid, _allocPoint);
+    }
+
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    function updatePool(uint256 _pid) public returns (PoolInfo memory pool) {
+        pool = poolInfo[_pid];
+        if (block.timestamp > pool.lastRewardTime) {
+            uint256 lpSupply = lpToken[_pid].balanceOf(address(this));
+            if (lpSupply > 0) {
+                uint256 time = block.timestamp - pool.lastRewardTime;
+                uint256 reward = time * rewardPerSecond * pool.allocPoint / totalAllocPoint;
+                pool.accRewardPerShare = pool.accRewardPerShare + (reward * ACC_REWARD_PRECISION / lpSupply);
+            }
+            pool.lastRewardTime = block.timestamp;
+            poolInfo[_pid] = pool;
+            emit LogUpdatePool(_pid, pool.lastRewardTime, lpSupply, pool.accRewardPerShare);
+        }
+    }
+
+    function deposit(uint256 pid, uint256 amount, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][to];
+
+        user.amount = user.amount + amount;
+        user.rewardDebt = user.rewardDebt + int256(amount * pool.accRewardPerShare / ACC_REWARD_PRECISION);
+
+        lpToken[pid].safeTransferFrom(msg.sender, address(this), amount);
+
+        emit Deposit(msg.sender, pid, amount, to);
+    }
+
+    function withdraw(uint256 pid, uint256 amount, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][msg.sender];
+
+        require(user.amount >= amount, "withdraw: not good");
+
+        user.rewardDebt = user.rewardDebt - int256(amount * pool.accRewardPerShare / ACC_REWARD_PRECISION);
+        user.amount = user.amount - amount;
+
+        lpToken[pid].safeTransfer(to, amount);
+
+        emit Withdraw(msg.sender, pid, amount, to);
+    }
+
+    function harvest(uint256 pid, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][msg.sender];
+        int256 accumulatedReward = int256(user.amount * pool.accRewardPerShare / ACC_REWARD_PRECISION);
+        uint256 _pendingReward = uint256(accumulatedReward - user.rewardDebt);
+
+        user.rewardDebt = accumulatedReward;
+
+        if (_pendingReward != 0) {
+            REWARD.safeTransfer(to, _pendingReward);
+        }
+
+        emit Harvest(msg.sender, pid, _pendingReward);
+    }
+
+    function withdrawAndHarvest(uint256 pid, uint256 amount, address to) public {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][msg.sender];
+
+        require(user.amount >= amount, "withdraw: not good");
+
+        int256 accumulatedReward = int256(user.amount * pool.accRewardPerShare / ACC_REWARD_PRECISION);
+        uint256 _pendingReward = uint256(accumulatedReward - user.rewardDebt);
+
+        user.rewardDebt = accumulatedReward - int256(amount * pool.accRewardPerShare / ACC_REWARD_PRECISION);
+        user.amount = user.amount - amount;
+        
+        REWARD.safeTransfer(to, _pendingReward);
+        lpToken[pid].safeTransfer(to, amount);
+
+        emit Withdraw(msg.sender, pid, amount, to);
+        emit Harvest(msg.sender, pid, _pendingReward);
+    }
+
+    function emergencyWithdraw(uint256 pid, address to) public {
+        UserInfo storage user = userInfo[pid][msg.sender];
+        uint256 amount = user.amount;
+        user.amount = 0;
+        user.rewardDebt = 0;
+
+        lpToken[pid].safeTransfer(to, amount);
+        emit EmergencyWithdraw(msg.sender, pid, amount, to);
+    }
+
+    /// @notice View function to see pending REWARD on frontend.
+    /// @param _pid The index of the pool. See `poolInfo`.
+    /// @param _user Address of user.
+    /// @return pending Reward for a given user.
+    function pendingReward(uint256 _pid, address _user) external view returns (uint256 pending) {
+        PoolInfo memory pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accRewardPerShare = pool.accRewardPerShare;
+        uint256 lpSupply = lpToken[_pid].balanceOf(address(this));
+        if (block.timestamp > pool.lastRewardTime && lpSupply != 0) {
+            uint256 time = block.timestamp - pool.lastRewardTime;
+            uint256 reward = time * rewardPerSecond * pool.allocPoint / totalAllocPoint;
+            accRewardPerShare = accRewardPerShare + reward * ACC_REWARD_PRECISION / lpSupply;
+        }
+        pending = uint256(int256(user.amount * accRewardPerShare / ACC_REWARD_PRECISION) - user.rewardDebt);
+    }
+
+    function setRewardPerSecond(uint256 _rewardPerSecond) public onlyOwner {
+        rewardPerSecond = _rewardPerSecond;
+        emit LogRewardPerSecond(_rewardPerSecond);
+    }
+
+    
+
+    function acceptOwnership() public {
+        // Checks
+        require(msg.sender == pendingOwner, "Ownable: caller != pending owner");
+
+        // Effects
+        emit OwnershipTransferred(owner, msg.sender);
+        owner = msg.sender;
+        pendingOwner = address(0);
+    }
+}
